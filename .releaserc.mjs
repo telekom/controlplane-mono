9ca5dbd3b0a339b64e19b19071feb3407c50890d
Ron Gummich <ron.gummich@outlook.de>
2025-05-23 13:57:47 +0200

    fix(semantic-release): changed order of plugins

--- .releaserc.mjs
+++ .releaserc.mjs
@@ -15,15 +15,15 @@ export default {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
+        ['@semantic-release/exec', {
+            prepareCmd: `bash ./update_install.sh "\${nextRelease.gitTag}"`,
+            publishCmd: `echo "\${nextRelease.notes}" > /tmp/release-notes.md`,
+        }],
         ['@semantic-release/git', {
             assets: [
                 'CHANGELOG.md',
                 'deploy/kustomization.yaml',
             ],
-        }],
-        ['@semantic-release/exec', {
-            prepareCmd: `bash ./update_install.sh "\${nextRelease.gitTag}"`,
-            publishCmd: `echo "\${nextRelease.notes}" > /tmp/release-notes.md`,
         }],
     ],
 };
